a7eec60824018a171ff29201fb58621ecc3b2310
Ethan Lee-Tyson <eleetyson@gmail.com>
2020-07-08 14:04:23 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ def key_for_min_value(name_hash)
   lowest_name = []
   lowest_val = 0
   name_hash.each do |name, val|
-    if lowest.empty?
+    if lowest_name.empty?
       lowest_name << name
       lowest_val = val
     else
